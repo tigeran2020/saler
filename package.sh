b6bef3bf5a51c796bc4ba8133822633a677c4351
tigeran <tigeran@tencent.com>
2020-10-25 18:22:40 +0800

    update package name

--- package.sh
+++ package.sh
@@ -1,7 +1,7 @@
 PLATFORM=x86_64-pc-windows-gnu
 VERSION=1.0.0
 
-ZIP_NAME=$PLATFORM-$VERSION.zip
+ZIP_NAME=fortune-$VERSION.zip
 
 rm -f $ZIP_NAME
 
@@ -11,10 +11,10 @@ RUN_SCRIPT_PATH=./tmp/run.bat
 # rustup target add $PLATFORM
 cargo build --target=$PLATFORM --release
 
-echo "qlion src.xls AX199" > $RUN_SCRIPT_PATH
+echo "fortune src.xls AX199" > $RUN_SCRIPT_PATH
 echo "pause" >> $RUN_SCRIPT_PATH
 
-cp target/$PLATFORM/release/qlion.exe ./tmp/
+cp target/$PLATFORM/release/qlion.exe ./tmp/fortune.exe
 cp testdatas/src.xls ./tmp/
 
 zip -q -r -D -j $ZIP_NAME tmp/ 
